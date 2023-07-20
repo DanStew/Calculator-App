728a92fbacf839a4c81b595df37bcc3d8cd8e03b
DanStew <90531180+DanStew@users.noreply.github.com>
2023-07-20 13:20:07 +0100

    Finalising Design of the Calculator

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+let calculatorButtons = document.querySelectorAll(".calculator_button")
+console.log(calculatorButtons)
+
+for (let i=0; i<calculatorButtons.length; i++){
+    calculatorButtons[i].addEventListener("click",processClick)
+}
+
+function processClick(e){
+    console.log(e)
+}
